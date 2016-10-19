7978f40f7a40898cae07839fa6648193e220eb9f
yangqlios <843111509@qq.com>
2016-10-19 20:07:42 +0800

    聊天信息DTO

---
+++ airmobile/chatRecordModel.h
@@ -0,0 +1,15 @@
+//
+//  chatRecordModel.h
+//  airmobile
+//
+//  Created by xuesong on 16/10/19.
+//  Copyright © 2016年 杨泉林. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface chatRecordModel : NSObject
+
+@property (nonatomic, copy) NSString *text;
+@property (nonatomic, copy) NSString *name;
+@end
