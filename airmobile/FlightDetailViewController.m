21af01617f97865effb3cdce1aeb2f570ac0a838
yangqlios <843111509@qq.com>
2016-10-19 20:01:08 +0800

    修改航班详情ViewController

--- airmobile/FlightDetailViewController.m
+++ airmobile/FlightDetailViewController.m
@@ -10,16 +10,28 @@
 #import "FlightDetailTableViewCell.h"
 #import "AbnormalityReportViewController.h"
 #import "FlightDetailSafeguardTableViewCell.h"
+#import "FlightDetailAirLineCollectionViewCell.h"
 
 static const NSString *FLIGHTDETAIL_TABLECELL_IDENTIFIER = @"FLIGHTDETAIL_TABLECELL_IDENTIFIER";
 static const NSString *FLIGHTDETAIL_SAFEGUARDTABLECELL_IDENTIFIER = @"FLIGHTDETAIL_SAFEGUARDTABLECELL_IDENTIFIER";
-@interface FlightDetailViewController ()<UITableViewDataSource,UITableViewDelegate,FlightDetailTableViewCellDelegate>
-@property (weak, nonatomic) IBOutlet UITableView *tableView;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *tableViewHeight;
-@property (weak, nonatomic) IBOutlet UITableView *safeguardTableView;
-@property (nonatomic, copy) NSArray *safeguardTableViewArray;
+static const NSString * FLIGHTDETAIL_AIRLINECOLLECTION_IDENTIFIER = @"FLIGHTDETAIL_AIRLINECOLLECTION_IDENTIFIER";
 
 
+@interface FlightDetailViewController ()<UITableViewDataSource,
+                                        UITableViewDelegate,
+                                        FlightDetailTableViewCellDelegate,
+                                        UICollectionViewDelegate,
+                                        UICollectionViewDataSource,
+                                        UICollectionViewDelegateFlowLayout>
+
+@property (weak, nonatomic) IBOutlet    UITableView         *tableView;
+@property (weak, nonatomic) IBOutlet    NSLayoutConstraint  *tableViewHeight;
+@property (weak, nonatomic) IBOutlet    UITableView         *safeguardTableView;
+@property (weak, nonatomic) IBOutlet    UICollectionView    *AirlineCollectionView;
+
+@property (nonatomic, copy) NSArray     *safeguardTableViewArray;
+@property (nonatomic, copy) NSArray     *airLineCollectionArray;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *safeguradViewHeight;
 
 @end
 
@@ -30,18 +42,29 @@ static const NSString *FLIGHTDETAIL_SAFEGUARDTABLECELL_IDENTIFIER = @"FLIGHTDETA
     
     //titleView订制
     [self titleViewInitWithHight:64];
-    self.titleView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"home_title_bg.png"]];
-    [self titleViewAddTitleText:@"航班列表"];
-    
+    [self titleViewAddTitleText:@"航班详情"];
     [self titleViewAddBackBtn];
     
-    _tableView.dataSource = self;
-    _tableView.delegate = self;
-    _tableViewHeight.constant = 1000;
-    _tableView.allowsSelection = NO;
-    _safeguardTableView.dataSource = self;
-    _safeguardTableView.delegate = self;
+    self.titleView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"home_title_bg.png"]];
+    
+    _tableView.dataSource       = self;
+    _tableView.delegate         = self;
+    _tableViewHeight.constant   = 1000;
+    _tableView.allowsSelection  = NO;
+    
+    _safeguardTableView.dataSource      = self;
+    _safeguardTableView.delegate        = self;
     _safeguardTableView.allowsSelection = NO;
+    
+    _AirlineCollectionView.delegate     = self;
+    _AirlineCollectionView.dataSource   = self;
+    
+    _airLineCollectionArray     = @[@"上海虹桥",@"深圳宝安",@"北京首都"];
+    _safeguardTableViewArray    = @[@"1",@""];
+    
+    
+    [_AirlineCollectionView registerNib:[UINib nibWithNibName:@"FlightDetailAirLineCollectionViewCell" bundle:nil]
+             forCellWithReuseIdentifier:(NSString *)FLIGHTDETAIL_AIRLINECOLLECTION_IDENTIFIER];
     
 }
 
@@ -94,6 +117,52 @@ static const NSString *FLIGHTDETAIL_SAFEGUARDTABLECELL_IDENTIFIER = @"FLIGHTDETA
     [self.navigationController pushViewController:abnormalityReportVC animated:YES];
 }
 
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return _airLineCollectionArray.count;
+}
+
+-(CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return CGSizeMake((kScreenWidth-59)/_airLineCollectionArray.count, collectionView.frame.size.height);
+}
+
+-(CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section
+{
+    return 0;
+}
+-(CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section
+{
+    return 0;
+}
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    FlightDetailAirLineCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:(NSString *)FLIGHTDETAIL_AIRLINECOLLECTION_IDENTIFIER forIndexPath:indexPath];
+    if (indexPath.row == 0) {
+        cell.type = FlightDetailAirLineCollectionViewCellTypeFirst;
+    }else if(indexPath.row == _airLineCollectionArray.count-1){
+        cell.type = FlightDetailAirLineCollectionViewCellTypeLast;
+    }
+    cell.titleNameLabel.text = _airLineCollectionArray[indexPath.row];
+    return cell;
+}
+
+- (IBAction)safeguradContractButtonClick:(UIButton *)sender {
+    if (sender.tag == 0) {
+        sender.tag = 1;
+        [UIView animateWithDuration:0.3 animations:^{
+            _safeguradViewHeight.constant = 30;
+            [self.view layoutIfNeeded];
+        }];
+    }else{
+        sender.tag = 0;
+        [UIView animateWithDuration:0.3 animations:^{
+            _safeguradViewHeight.constant = _safeguardTableViewArray.count *30+30;
+            [self.view layoutIfNeeded];
+        }];
+    }
+}
 
 /*
 #pragma mark - Navigation
