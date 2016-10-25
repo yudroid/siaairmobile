4229f7468e68d6358647f5a48657f679d8a2d067
yangqlios <843111509@qq.com>
2016-10-25 15:48:26 +0800

    讲通讯录和值班表迁移到功能模块

--- airmobile/controller/FunctionViewController.m
+++ airmobile/controller/FunctionViewController.m
@@ -11,8 +11,16 @@
 #import "HomePageViewController.h"
 #import "MessageViewController.h"
 #import "UserInfoViewController.h"
+#import "UserInfoTableViewCell.h"
+#import "NightShiftRoomViewController.h"
+#import "AddressBookViewController.h"
 
-@interface FunctionViewController ()
+static const NSString *FUNCTION_TABLECELL_IDENTIFIER = @"FUNCTION_TABLECELL_IDENTIFIER";
+
+@interface FunctionViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic,strong) UITableView *tableView;
+    @property (nonatomic,strong) NSArray *tableArray;
 
 @end
 
@@ -20,15 +28,66 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+
+    [self initTitleView];
+    [self initTable];
     //TabBer自定义
     self.tabBarView = [[TabBarView alloc] initTabBarWithModel:TabBarBgModelHomePage selectedType:TabBarSelectedTypeFunction delegate:self];
     [self.view insertSubview:self.tabBarView aboveSubview:self.view];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)initTitleView
+{
+    //titleView订制
+    [self titleViewInitWithHight:64];
+    self.titleView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"home_title_bg.png"]];
+    [self titleViewAddTitleText:@"功能"];
+}
+-(void)initTable
+{
+    _tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 64, kScreenWidth, kScreenHeight-64-49)
+                                             style:UITableViewStylePlain];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    _tableView.backgroundColor = [UIColor clearColor];
+    [_tableView registerNib:[UINib nibWithNibName:@"UserInfoTableViewCell" bundle:nil] forCellReuseIdentifier:(NSString *)FUNCTION_TABLECELL_IDENTIFIER];
+    _tableView.tableFooterView = [[UIView alloc]init];
+    _tableArray= @[@"通讯录",@"值班表"];
+
+    [self.view addSubview:_tableView];
+}
+
+
+#pragma mark - UITableViewDelegate UITableViewDataSource
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _tableArray.count;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 100;
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UserInfoTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:(NSString *)FUNCTION_TABLECELL_IDENTIFIER];
+    cell.nameLabel.text = _tableArray[indexPath.row];
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+
+    NSString *name = _tableArray[indexPath.row];
+    if ([name isEqualToString:@"值班表"]){
+        NightShiftRoomViewController *nightShiftRoomVC = [[NightShiftRoomViewController alloc]initWithNibName:@"NightShiftRoomViewController" bundle:nil];
+        [self.navigationController pushViewController:nightShiftRoomVC animated:YES];
+    }else if ([name isEqualToString:@"通讯录"]){
+        AddressBookViewController *addressBookVC = [[AddressBookViewController alloc]initWithNibName:@"AddressBookViewController" bundle:nil];
+        [self.navigationController pushViewController:addressBookVC animated:YES];
+    }
 }
 
 #pragma mark - 切换底部主功能页面
