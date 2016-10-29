eaee01d3492a035c74aba6d21b851fa2a1fd01ed
yangqlios <843111509@qq.com>
2016-10-29 10:08:28 +0800

    添加异常上报tablecell 样式

---
+++ airmobile/uiview/AbnormalityReportHistoryTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  AbnormalityReportHistoryTableViewCell.h
+//  airmobile
+//
+//  Created by xuesong on 16/10/29.
+//  Copyright © 2016年 杨泉林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AbnormalityReportHistoryTableViewCell : UITableViewCell
+
+@end
---
+++ airmobile/uiview/AbnormalityReportHistoryTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  AbnormalityReportHistoryTableViewCell.m
+//  airmobile
+//
+//  Created by xuesong on 16/10/29.
+//  Copyright © 2016年 杨泉林. All rights reserved.
+//
+
+#import "AbnormalityReportHistoryTableViewCell.h"
+
+@implementation AbnormalityReportHistoryTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
