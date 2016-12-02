f70e194ad0ba502faadcab166b40bdaee887d0ce
yangqlios <yangql_ios@163.com>
2016-12-02 17:32:41 +0800

    修改映射关系

--- airmobile/model/FlightStusModel.m
+++ airmobile/model/FlightStusModel.m
@@ -31,23 +31,23 @@
     if([self isNull:data])
         return;
     
-    _flightCount = [[data objectForKey:@"flightCount"] intValue];
-    _arrCount = [[data objectForKey:@"arrCount"] intValue];
-    _depCount = [[data objectForKey:@"depCount"] intValue];
+    _flightCount = [[data objectForKey:@"planTotal"] intValue];
+    _arrCount = [[data objectForKey:@"planIn"] intValue];
+    _depCount = [[data objectForKey:@"planOut"] intValue];
     
-    _arrDoneNormal = [[data objectForKey:@"arrDoneNormal"] intValue];
-    _arrDoneAbn = [[data objectForKey:@"arrDoneAbn"] intValue];
-    _arrPlanNormal = [[data objectForKey:@"arrPlanNormal"] intValue];
-    _arrPlanAbn = [[data objectForKey:@"arrPlanAbn"] intValue];
-    _arrDelay = [[data objectForKey:@"arrDelay"] intValue];
-    _arrCancel = [[data objectForKey:@"arrCancel"] intValue];
+    _arrDoneNormal = [[data objectForKey:@"inFinished"] intValue];
+    _arrDoneAbn = [[data objectForKey:@"inFinishedDelay"] intValue];
+    _arrPlanNormal = [[data objectForKey:@"inNoFinished"] intValue];
+    _arrPlanAbn = [[data objectForKey:@"inNoFinishedDelay"] intValue];
+    _arrDelay = [[data objectForKey:@"inDelay"] intValue];
+    _arrCancel = [[data objectForKey:@"inCancel"] intValue];
     
-    _depDoneNormal = [[data objectForKey:@"depDoneNormal"] intValue];
-    _depDoneAbn = [[data objectForKey:@"depDoneAbn"] intValue];
-    _depPlanNormal = [[data objectForKey:@"depPlanNormal"] intValue];
-    _depPlanAbn = [[data objectForKey:@"depPlanAbn"] intValue];
-    _depDelay = [[data objectForKey:@"depDelay"] intValue];
-    _depCancel = [[data objectForKey:@"depCancel"] intValue];
+    _depDoneNormal = [[data objectForKey:@"outFinished"] intValue];
+    _depDoneAbn = [[data objectForKey:@"outFinishedDelay"] intValue];
+    _depPlanNormal = [[data objectForKey:@"outNoFinished"] intValue];
+    _depPlanAbn = [[data objectForKey:@"outNoFinishedDelay"] intValue];
+    _depDelay = [[data objectForKey:@"outDelay"] intValue];
+    _depCancel = [[data objectForKey:@"outCancel"] intValue];
 
 }
 
